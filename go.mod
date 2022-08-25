7aee03d2cc102e1ad206efde84718937cddf268c
b4b4r07 <b4b4r07@gmail.com>
2022-08-25 11:51:27 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/b4b4r07/github-approve
+
+go 1.18
+
+require (
+	github.com/b4b4r07/go-cli-log v0.0.0-20200124120248-8fac4d71de01
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/jessevdk/go-flags v1.5.0
+	golang.org/x/net v0.0.0-20220822230855-b0a4917ee28c
+	golang.org/x/oauth2 v0.0.0-20220822191816-0ebed06d0094
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/logutils v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	clilog "github.com/b4b4r07/go-cli-log"
+	"github.com/google/go-github/github"
+	"github.com/jessevdk/go-flags"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+)
+
+// These variables are set in build step
+var (
+	Version  = "unset"
+	Revision = "unset"
+)
+
+// CLI represents this application itself
+type CLI struct {
+	Option Option
+	Stdout io.Writer
+	Stderr io.Writer
+}
+
+// Option represents application options
+type Option struct {
+	Owner  string `long:"owner" description:"owner" required:"true"`
+	Repo   string `long:"repo" description:"repo" required:"true"`
+	Number int    `long:"number" description:"number" required:"true"`
+	Event  string `long:"event" description:"review event" choice:"APPROVE" choice:"REQUEST_CHANGES" choice:"COMMENT" default:"APPROVE"`
+}
+
+func main() {
+	os.Exit(run(os.Args[1:]))
+}
+
+func run(args []string) int {
+	clilog.Env = "LOG"
+	clilog.SetOutput()
+	defer log.Printf("[INFO] finish main function")
+
+	log.Printf("[INFO] Version: %s (%s)", Version, Revision)
+	log.Printf("[INFO] Args: %#v", args)
+
+	var opt Option
+	args, err := flags.ParseArgs(&opt, args)
+	if err != nil {
+		return 2
+	}
+
+	cli := CLI{
+		Option: opt,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+
+	if err := cli.Run(args); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return 1
+	}
+
+	return 0
+}
+
+func (c *CLI) Run(args []string) error {
+	token := os.Getenv("GITHUB_TOKEN")
+	if len(token) == 0 {
+		return errors.New("GITHUB_TOKEN is missing")
+	}
+
+	// Construct github HTTP client
+	ts := oauth2.StaticTokenSource(&oauth2.Token{
+		AccessToken: token,
+	})
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	tc.Transport = clilog.NewTransport("github", tc.Transport)
+	client := github.NewClient(tc)
+
+	review, _, err := client.PullRequests.CreateReview(
+		context.Background(),
+		c.Option.Owner,
+		c.Option.Repo,
+		c.Option.Number,
+		// https://docs.github.com/en/rest/pulls/reviews#create-a-review-for-a-pull-request
+		&github.PullRequestReviewRequest{
+			Event: github.String(c.Option.Event),
+		},
+	)
+	if err != nil {
+		log.Printf("[ERROR] Failed to create review")
+		return err
+	}
+
+	log.Printf("[INFO] Successfully created a review!")
+	log.Printf("[TRACE] response: %v", review)
+	return nil
+}
